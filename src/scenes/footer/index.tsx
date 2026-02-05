36fbfbcdedbd079f6044023b3c7bb3f10fbd4b7d
SimretAbebe <simretabebe24@gmail.com>
2026-02-05 18:37:54 +0300

    chnaging footers

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -21,7 +21,7 @@ const Footer = () => {
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold text-mono-accent">Join Us</h4>
           <p className="my-5">Start your fitness journey with us. Inquire about classes and membership.</p>
-          <p>+251 9XX XXX XXXX</p>
+          <p>Reach Out Us any time u want</p>
         </div>
       </div>
     </footer>
